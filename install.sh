b922af433ecb9edbdc303bba6967a418c533681b
Matheus Sampaio <matheus@sampaio.us>
2021-04-07 10:56:10 -0500

    feat: update scripts to work on osx

--- install.sh
+++ install.sh
@@ -1,31 +1,17 @@
 #! /bin/bash
 
-set -ex
+set -e
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2) Greetings. Preparing to power up and begin diagnostics.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
-INSTALLDIR=$PWD
-
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2) Creating symbolic links.$(tput sgr 0)"
+echo "$(tput setaf 2) Installing oh-my-zsh.$(tput sgr 0)"
 echo "---------------------------------------------------------"
-
-ln -fs $PWD/zsh/.zshrc $HOME/.zshrc
-ln -fs $PWD/zsh/.zshenv $HOME/.zshenv
-
-ln -fs $PWD/tmux/.tmux.conf $HOME/.tmux.conf
-
-mkdir -p $XDG_CONFIG_HOME/git
-ln -fs $PWD/git/.gitignore $XDG_CONFIG_HOME/git/ignore
-ln -fs $PWD/git/.gitconfig $HOME/.gitconfig
-
-mkdir -p $HOME/.config/nvim
-
-ln -fs $PWD/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
-ln -fs $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -fs $PWD/nvim/after $HOME/.config/nvim/after
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   ./install_osx.sh
@@ -80,10 +66,22 @@ fi
 $HOME/.fzf/install --all --no-bash --no-fish --xdg
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2) Installing oh-my-zsh.$(tput sgr 0)"
+echo "$(tput setaf 2) Creating symbolic links.$(tput sgr 0)"
 echo "---------------------------------------------------------"
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+
+ln -fs $PWD/zsh/.zshrc $HOME/.zshrc
+ln -fs $PWD/zsh/.zshenv $HOME/.zshenv
+
+ln -fs $PWD/tmux/.tmux.conf $HOME/.tmux.conf
+
+mkdir -p $XDG_CONFIG_HOME/git
+ln -fs $PWD/git/.gitignore $XDG_CONFIG_HOME/git/ignore
+ln -fs $PWD/git/.gitconfig $HOME/.gitconfig
+
+mkdir -p $HOME/.config/nvim
+
+ln -fs $PWD/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
+ln -fs $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -fs $PWD/nvim/after $HOME/.config/nvim/after
 
 exit 0
