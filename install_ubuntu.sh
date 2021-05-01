2b06ac1ee8b397e294535703908943c659341f27
Matheus Sampaio <matheus@sampaio.us>
2021-05-01 00:06:29 -0500

    feat: update ubuntu and osx libraries to install

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -15,18 +15,17 @@ sudo apt-get install -y build-essential gcc curl git
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2) Install tools.$(tput sgr 0)"
 echo "---------------------------------------------------------"
-sudo apt-get install -y file python3 tmux ripgrep htop hub zsh tree highlight file w3m
+sudo apt-get install -y git python3 python3-pip tmux zsh ripgrep htop hub tree wget jq fonts-powerline
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2) Install node.$(tput sgr 0)"
 echo "---------------------------------------------------------"
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2) Installing NeoVim.$(tput sgr 0)"
-echo " ---------------------------------------------------------"
-# sudo apt-get install -y python-neovim python3-neovim neovim
-wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
-sudo mv nvim.appimage /usr/bin/nvim
-sudo chmod +x /usr/bin/nvim
+echo "$(tput setaf 2) Installing laztgit.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+sudo add-apt-repository ppa:lazygit-team/release
+sudo apt-get update
+sudo apt-get install -y lazygit
