e8d01b2ba439733466834617c567ea7b52c5a8c6
Matheus Sampaio <matheus@sampaio.us>
2022-08-15 16:17:08 -0700

    feat: back to latest packer release

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -222,7 +222,7 @@ local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.n
 local packer_bootstrap = false
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    packer_bootstrap = vim.fn.system('git clone --branch keys-desc --depth 1 https://github.com/wbthomason/packer.nvim ' .. install_path)
+    packer_bootstrap = vim.fn.system('git clone --depth 1 https://github.com/wbthomason/packer.nvim ' .. install_path)
 
     vim.cmd('packadd packer.nvim')
 end
